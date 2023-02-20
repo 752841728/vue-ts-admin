e0ff7a101b578f3dcf66cd7f94adc11e3eb087bc
陈颖 <59192839+752841728@users.noreply.github.com>
2023-02-20 14:24:28 +0800

    Initialize perf/home

---
+++ src/router/modules/home.ts
@@ -0,0 +1,26 @@
+import { $t } from "@/plugins/i18n";
+import { home } from "@/router/enums";
+const Layout = () => import("@/layout/index.vue");
+
+export default {
+  path: "/",
+  name: "Home",
+  component: Layout,
+  redirect: "/welcome",
+  meta: {
+    icon: "homeFilled",
+    title: $t("menus.hshome"),
+    rank: home,
+    showLink: false
+  },
+  children: [
+    {
+      path: "/welcome",
+      name: "Welcome",
+      component: () => import("@/views/welcome/index.vue"),
+      meta: {
+        title: $t("menus.hshome")
+      }
+    }
+  ]
+} as RouteConfigsTable;
