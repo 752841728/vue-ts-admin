2c77cd452cd09b79855fdcedbbfe2e09bb52448f
752841728 <59192839+752841728@users.noreply.github.com>
2023-02-20 06:25:57 +0000

    Deploying to gh-pages from @ 752841728/vue-ts-admin@7b21ea30a31754dc2c575ccd5a5de08b4546c321 🚀

---
+++ static/js/system-b63b5d72.js
@@ -0,0 +1,1 @@
+import{g as e}from"./index-d7b5a34d.js";const r=t=>e.request("post","/user",{data:t}),o=t=>e.request("post","/role",{data:t}),p=t=>e.request("post","/dept",{data:t});export{o as a,r as b,p as g};
